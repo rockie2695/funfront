c8f5736c3e40874bb086108127fd38b7317b4e44
rockie2695 <21993381+rockie2695@users.noreply.github.com>
2019-05-08 17:55:50 +0800

    Add files via upload

--- src/Character.js
+++ src/Character.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button, Form, Container, Segment, List, Table } from "semantic-ui-react"
 import EachCharacter from "./EachCharacter"
+import className from "classnames"
 class Character extends Component {
     _isMounted = false;
     constructor(props) {
@@ -9,9 +10,13 @@ class Character extends Component {
             inputLoading: null,
             deleteLoading: null,
             text: "",
-            characters: [{ _id: 123, name: 777, lv: 0, mana: 0, blood: 0, phy: 0, soul: 0, time: 0, magic: 0, updateCancelHide: "hide", editHide: null, }]
+            characters: [{ _id: 123, name: 777, lv: 0, mana: 0, blood: 0, phy: 0, soul: 0, time: 0, magic: 0, updateCancelHide: "hide", editHide: null, updateLoading: null }]
         }
+        this.changeText = this.changeText.bind(this)
         this.delete = this.delete.bind(this)
+        this.edit = this.edit.bind(this)
+        this.update = this.update.bind(this)
+        this.cancelUpdate = this.cancelUpdate.bind(this)
     }
     componentDidMount() {
         this._isMounted = true;
@@ -19,9 +24,9 @@ class Character extends Component {
     componentWillUnmount() {
         this._isMounted = false;
     }
-    changeText(text) {
-        if (this.state.text !== text) {
-            this.setState({ text: text })
+    changeText(event) {
+        if (this.state.text !== event.target.value) {
+            this.setState({ text: event.target.value })
         }
     }
     changeToLoad() {
@@ -36,10 +41,13 @@ class Character extends Component {
             })
                 .then(res => res.json())
                 .then((result) => {
-                    console.log(result)
                     if (this._isMounted) {
                         if (typeof result.ok != "undefined") {
-                            this.setState(prevState => ({ characters: [result.ok, ...prevState.characters] }))
+                            result.ok.updateCancelHide = "hide"
+                            result.ok.editHide = null
+                            result.ok.updateLoading = null
+                            this.setState(prevState => ({ characters: [result.ok, ...prevState.characters,] }))
+                            this.setState({ text: "" })
                         } else if (typeof result.error != "undefined") {
                             console.log(result.error)
                             alert(result.error)
@@ -56,6 +64,61 @@ class Character extends Component {
                     }
                 });
         }
+    }
+    edit(_id) {//https://stackoverflow.com/questions/54382559/how-to-set-state-for-a-specific-object-in-an-array-in-react
+        this.setState(prev => ({
+            characters: prev.characters.map(character => character._id === _id ? { ...character, updateCancelHide: null, editHide: "hide" } : character)
+        }))
+        //other answer https://stackoverflow.com/questions/29537299/react-how-do-i-update-state-item1-on-setstate-with-jsfiddle
+    }
+    cancelUpdate(_id) {
+        this.setState(prev => ({
+            characters: prev.characters.map(character => character._id === _id
+                ? { ...character, updateCancelHide: "hide", editHide: null/*, lv: character.lv, blood: character.blood, mana: character.mana, phy: character.phy, magic: character.magic, soul: character.soul */ } : character)
+        }))
+    }
+    update(updateCharacter) {
+        console.log(updateCharacter)
+        this.setState((prev) => ({
+            characters: prev.characters.map(character => character._id === updateCharacter._id ? { ...character, updateLoading: "loading" } : character)
+        }))
+        let query = {
+            lv: updateCharacter.lv,
+            blood: updateCharacter.blood,
+            mana: updateCharacter.mana,
+            phy: updateCharacter.phy,
+            magic: updateCharacter.magic,
+            soul: updateCharacter.soul
+        }
+        let whereCon = { "_id": updateCharacter._id }
+        fetch('http://localhost:8080/update/character', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                "query": query,
+                "whereCon": whereCon
+            })
+        })
+            .then(res => res.json())
+            .then((result) => {
+                if (typeof result.ok != "undefined") {
+                    //this.setState(prevState => ({ characters: [result.ok, ...prevState.characters] }))
+                    console.log(result.ok)
+                    let newCharacter = result.ok
+                    this.setState((prev) => ({
+                        characters: prev.characters.map(character => character._id === newCharacter._id ? { ...character, updateCancelHide: "hide", editHide: null, updateLoading: null, ...newCharacter } : character)
+                    }))
+                    console.log(this.state.characters)
+                } else if (typeof result.error != "undefined") {
+                    console.log(result.error)
+                    alert(result.error)
+                } else {
+                    alert("unknow error")
+                }
+            }, (error) => {
+                console.log(error)
+                alert(error)
+            })
     }
     delete(_id) {
         fetch('http://localhost:8080/delete/character', {
@@ -105,7 +168,7 @@ class Character extends Component {
                 <Table.Body>
                     {this.state.characters.map((character) =>
                         <EachCharacter key={character._id}
-                            value={character} delete={this.delete} />
+                            value={character} delete={this.delete} update={this.update} edit={this.edit} cancelUpdate={this.cancelUpdate} />
                     )}
                 </Table.Body>
 
@@ -114,7 +177,7 @@ class Character extends Component {
             <Form className={this.state.inputLoading}>
                 <Form.Field>
                     <label>Name</label>
-                    <input placeholder='Name' required onChange={(event) => { this.changeText(event.target.value) }} />
+                    <input placeholder='Name' required value={this.state.text} onChange={this.changeText} />
                 </Form.Field>
                 <Button type='submit' onClick={() => { this.changeToLoad() }}>Submit</Button>
             </Form>
